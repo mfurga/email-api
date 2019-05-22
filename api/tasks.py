fb7084bdd98102f3af64be576c400fc3487390a8
Mateusz Furga <matfurga@gmail.com>
2019-05-22 13:30:49 +0200

    Update send_mail task

--- api/tasks.py
+++ api/tasks.py
@@ -2,14 +2,15 @@ from django.utils.translation import ugettext as _
 from django.core.mail.backends.smtp import EmailBackend
 from django.core.mail import EmailMessage
 from django.utils import timezone
-from celery import shared_task
+from smtplib import SMTPException
 import logging
 
 from api.models import Email
+from app.celery import app
 
 
-@shared_task
-def send_mail(instance_id, mailbox, subject, text, to, cc=None, bcc=None,
+@app.task(bind=True, default_retry_delay=10, max_retries=3)
+def send_mail(self, instance_id, mailbox, subject, text, to, cc=None, bcc=None,
               reply_to=None, attachment=None):
     """
     The task used to send emails.
@@ -22,17 +23,15 @@ def send_mail(instance_id, mailbox, subject, text, to, cc=None, bcc=None,
     # Email content configuration
     email = EmailMessage(subject=subject, body=text, from_email=mailbox['from_email'],
                          to=to, cc=cc, bcc=bcc, connection=backend, reply_to=[reply_to])
-
     if attachment:
         email.attach_file(attachment)
 
-    tries = 0
-    while email.send(fail_silently=False) == 0:
-        if tries == 3:
-            logging.error(_('Failed to send the message.'))
-            return
-        tries += 1
-
-    instance = Email.objects.get(id=instance_id)
-    instance.sent_date = timezone.now()
-    instance.save()
+    try:
+        email.send(fail_silently=False)
+    except SMTPException as e:
+        logging.error(_('Failed to send the message. (%s)' % str(e)))
+        self.retry(exc=e)
+    else:
+        instance = Email.objects.get(id=instance_id)
+        instance.sent_date = timezone.now()
+        instance.save()
